a336d22a5fd756686d6b90a8fcf6c7ad547cb22d
Atul Sharma <atullsharma2000@gmail.com>
2020-07-26 19:58:37 +0530

    1

--- sendmail.py
+++ sendmail.py
@@ -1,6 +1,6 @@
 import smtplib
 s=smtplib.SMTP("smtp.gmail.com",587)
 s.starttls()
-s.login("awesome.asssassin01@gmail.com","insane@2003")
+s.login("awesome.assassin01@gmail.com","insane@2003")
 message=("everthing is working fine!!")
 s.sendmail("awesome.asssassin01@gmail.com","atullsharma2000@gmail.com",message)
